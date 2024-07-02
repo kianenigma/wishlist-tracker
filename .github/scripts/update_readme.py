bee9fbe9e731e247a440e22e767de0dcba1c9aea
kianenigma <kian@parity.io>
2024-07-02 17:34:29 +0330

    make it work

--- .github/scripts/update_readme.py
+++ .github/scripts/update_readme.py
@@ -6,7 +6,7 @@ import os
 g = Github(os.getenv('GH_TOKEN'))  # Use the token securely
 
 # Regex pattern to match wish format
-pattern = re.compile(r"I wish for (https://github\.com/([a-zA-Z0-9_.-]+)/([a-zA-Z0-9_.-]+)/issues/(\d+))")
+pattern = re.compile(r"I wish for:? (https://github\.com/([a-zA-Z0-9_.-]+)/([a-zA-Z0-9_.-]+)/issues/(\d+))")
 
 # Define the regular expression pattern for the leaderboard sections
 leaderboard_section_pattern_dev = re.compile(r"(<!-- LEADERBOARD_DEV:START -->).*?(<!-- LEADERBOARD_DEV:END -->)", re.DOTALL)
@@ -19,12 +19,12 @@ issues = {
 }
 
 # Tracker repo
-tracker_repo = g.get_repo("joshuacheong/wishlist-tracker")
+tracker_repo = g.get_repo("kianenigma/wishlist-tracker")
 
 # Markdown tables for each category
 md_tables = {
-    'Developer': "| Feature Request | Summary | Votes |\n| --- | --- | --- |\n",
-    'User': "| Feature Request | Summary | Votes |\n| --- | --- | --- |\n"
+    'Developer': "| Feature Request | Summary | Votes | Status |\n| --- | --- | --- | --- |\n",
+    'User': "| Feature Request | Summary | Votes | Status |\n| --- | --- | --- | --- |\n"
 }
 
 # Process each issue
@@ -35,26 +35,37 @@ for category, (repo_name, issue_number) in issues.items():
     issue_details = {}
 
     for comment in issue.get_comments():
-        matches = pattern.findall(comment.body)
+        # in the comment body, if there is a string `#(\d)`, replace it with
+        # https://github\.com/paritytech/polkadot-sdk/issues/(number)
+        updated_body = re.sub(r'#(\d+)', r"https://github.com/paritytech/polkadot-sdk/issues/\1", comment.body)
+
+        matches = pattern.findall(updated_body)
         for match in matches:
             url, org, repo_name, issue_id = match
             issue_key = (url, org, repo_name, issue_id)
             # Get the count of thumbs-up reactions for the comment
-            thumbs_up_count = sum(1 for reaction in comment.get_reactions() if reaction.content == '+1')
+            reactions = comment.get_reactions()
+            thumbs_up_count = sum(1 for reaction in reactions if reaction.content == '+1')
+            print(f"Found wish: {url} with {thumbs_up_count} +1s")
             if issue_key in wishes:
+                print("Incrementing count by ", thumbs_up_count)
                 wishes[issue_key] += thumbs_up_count
             else:
-                wishes[issue_key] = thumbs_up_count
                 # Fetch the issue to get the title
                 temp_repo = g.get_repo(f"{org}/{repo_name}")
                 temp_issue = temp_repo.get_issue(int(issue_id))
-                issue_details[url] = temp_issue.title  # or temp_issue.body for more detailed summary
+                temp_reactions = temp_issue.get_reactions()
+                issue_details[url] = [temp_issue.title, "👾 Open" if temp_issue.state == 'open' else "✅Closed"]
+                source_thumbs_up_count = sum(1 for reaction in temp_reactions if reaction.content == '+1')
+                print(f"initial count {thumbs_up_count} + {source_thumbs_up_count}")
+                wishes[issue_key] = thumbs_up_count + source_thumbs_up_count
 
     # Sort wishes by count and add to the markdown table
     sorted_wishes = sorted(wishes.items(), key=lambda x: x[1], reverse=True)
     for (url, _, _, _), count in sorted_wishes:
-        summary = issue_details.get(url, "No summary available")
-        md_tables[category] += f"| {url} | {summary} | {count} |\n"
+        [summary, status] = issue_details.get(url, "No summary available")
+        md_tables[category] += f"| {url} | {summary} | {count} | {status} |\n"
+    print(md_tables[category])
 
 # Fetch and update README.md
 readme = tracker_repo.get_contents("README.md")
